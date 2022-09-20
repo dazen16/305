053ce4bba7fc57aa94004d47963e4ee113cf27db
dazen16 <dzeng10@hotmail.com>
2022-09-20 14:07:19 -0700

    test2

--- src/test305/test.java
+++ src/test305/test.java
@@ -3,5 +3,9 @@ package test305;
 public class test {
     public static void main(String args[]){
         System.out.println("Hello World");
+
+        int a = 0;
+
+        System.out.println("test2");
     }
 }
